b2977bb5a0bc3e47d7291102ea2c3c9141a2ce60
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-29 18:27:13 +0200

    game schema

---
+++ src/game_logic/schema.js
@@ -0,0 +1,19 @@
+// Reference: https://developer.mozilla.org/en-US/docs/Web/CSS/CSSOM_View/Coordinate_systems#example
+// top left is 0, 0
+// x increases to the right, y increases down
+
+// board: {width: number, height: number}
+// coordinate: {x: number, y: number}
+// snake: [coordinate, coordinate, coordinate, ...]
+// food: coordinate
+// direction: "UP", "DOWN", "LEFT", or "RIGHT"
+
+// game: {
+//   board,
+//   snake,
+//   food,
+//   direction,
+// }
+
+// TODO: multiple snakes, multiple foods
+
