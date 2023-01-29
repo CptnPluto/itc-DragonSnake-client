f27f9691de21fe3d6f6b9a78b1664a399a6f78ec
CptnPluto <cptnpluto@gmail.com>
2023-01-29 16:16:15 +0200

    Added functionality to swap between logged in and logged out based on whether User is defined. Will need to update when context and BE is set up.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,55 @@
+import { useState } from "react";
+
 const Homepage = () => {
+    // To be replaced with values from context.
+    const [user, setUser] = useState();
+
     return (
         <>
-            <div className="header"></div>
-            <h2>Welcome to TotallyCoolName Snake Game!</h2>
-            <div className="main"></div>
-            <h3>Play the game here!</h3>
-            <h4>Instructions:</h4>
-            <div>
-                <p>Use the arrow keys to move the snake around the board.</p>
-            </div>
-            <div className="footer">
-                <h5>Created by: TotallyCoolName</h5>
+            <button onClick={() => setUser(null)}>Temp Logout</button>
+            <button
+                onClick={() =>
+                    setUser({
+                        nickname: "GroovyTuesday",
+                        scores: [1, 2, 3, 4, 5],
+                    })
+                }
+            >
+                Temp Login
+            </button>
+            {!user ? (
+                <>
+                    <div className="header">
+                        <h2>Welcome to TotallyCoolName Snake Game!</h2>
+                    </div>
+                    <div className="main"></div>
+                    <h3>Play the game here!</h3>
+                    <h4>Instructions:</h4>
+                    <div>
+                        <p>
+                            Use the arrow keys to move the snake around the
+                            board.
+                        </p>
+                    </div>
+                    <div className="footer">
+                        <h5>Created by: TotallyCoolName</h5>
+                    </div>
+                </>
+            ) : (
+                <div className="header">
+                    <h2>Welcome back, {user.nickname}!</h2>
+                    <div className="scoreboard">
+                        <h3>Scoreboard</h3>
+                        <ul>
+                            {user.scores.map((score, index) => (
+                                <li key={index}>{score}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+            <div className="start_game">
+                <button>Start Game</button>
             </div>
         </>
     );
