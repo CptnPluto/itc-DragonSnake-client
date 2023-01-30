be23f13b3f53a6164c5533fd2e9934c7c431a3a6
Alon <alonkerklies@gmail.com.com>
2023-01-30 17:19:53 +0200

    start game page and css

--- src/pages/Gamepage.js
+++ src/pages/Gamepage.js
@@ -1,9 +1,36 @@
 import React from "react";
+import './Gamepage.css'
+import gameField from "../images/gameField.jpg";
+
 
 const Gamepage = () => {
     return (
         // ALON - add your design here. I'll integrate it all together later.
-        <div>Gamepage</div>
+        <div className="gamepage-container">
+
+         <div className="sideList" style={{ border: "2px    solid #FFFFFF1C", borderRight: 0 }}>
+         <h2>Now Online</h2>
+         </div>
+
+
+         <div className="mainRight" style={{ border: "2px solid #FFFFFF1C" }}>
+                <div className="topBar">  
+                <h2>Score:</h2>
+                <h2>win:</h2>
+                <h2>coins:</h2>
+
+                <p className="userName">userName</p>
+                <div className="userPic" ></div>
+               </div>
+
+             < div className="gameField"> 
+              <img src={gameField} alt="gamePic" className="gamePic"/>
+              </div>
+
+         </div>
+
+
+        </div>
     );
 };
 
