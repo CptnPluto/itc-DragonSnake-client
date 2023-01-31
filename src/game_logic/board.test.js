197c984f62dde8cd3129cff9dd2673fdca4b78a8
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-31 08:58:54 +0200

    check insert and move

--- src/game_logic/board.test.js
+++ src/game_logic/board.test.js
@@ -1,4 +1,5 @@
 import { getMiddle, getRandomDirection, insertSnake } from "./board";
+import { move } from "./snake";
 
 describe("get middle coordinate of board", () => {
   test("3x3 board", () => {
@@ -24,9 +25,6 @@ describe("inserting snakes", () => {
   test("snake inserted at top left", () => {
     const ROWS = 3;
     const COLS = 3;
-    // BOARD SIZE (px) IN Game.css
-    const INITIAL_DIRECTION = "RIGHT";
-    const INITIAL_SPEED = 1000;
     const cells = [];
 
     for (let i = 0; i < ROWS; i++) {
@@ -43,21 +41,66 @@ describe("inserting snakes", () => {
 
     const snake = [
       { row: 0, col: 0 },
-      { row: 1, col: 0 },
-      { row: 2, col: 0 },
+      { row: 0, col: 1 },
+      { row: 0, col: 2 },
     ];
     const newBoard = insertSnake(cells, snake);
-    // console.log(newBoard);
     expect(newBoard).toEqual([
       { row: 0, col: 0, isTail: true, isHead: false, isFood: false },
-      { row: 0, col: 1, isTail: false, isHead: false, isFood: false },
-      { row: 0, col: 2, isTail: false, isHead: false, isFood: false },
-      { row: 1, col: 0, isTail: true, isHead: false, isFood: false },
+      { row: 0, col: 1, isTail: true, isHead: false, isFood: false },
+      { row: 0, col: 2, isTail: false, isHead: true, isFood: false },
+      { row: 1, col: 0, isTail: false, isHead: false, isFood: false },
       { row: 1, col: 1, isTail: false, isHead: false, isFood: false },
       { row: 1, col: 2, isTail: false, isHead: false, isFood: false },
-      { row: 2, col: 0, isTail: false, isHead: true, isFood: false },
+      { row: 2, col: 0, isTail: false, isHead: false, isFood: false },
       { row: 2, col: 1, isTail: false, isHead: false, isFood: false },
       { row: 2, col: 2, isTail: false, isHead: false, isFood: false },
+    ]);
+  });
+});
+
+describe("inserting and move snake", () => {
+  test("snake inserted at top left and moved right", () => {
+    const ROWS = 4;
+    const COLS = 4;
+    const cells = [];
+
+    for (let i = 0; i < ROWS; i++) {
+      for (let j = 0; j < COLS; j++) {
+        cells.push({
+          row: i,
+          col: j,
+          isHead: false,
+          isTail: false,
+          isFood: false,
+        });
+      }
+    }
+
+    const snake = [
+      { row: 0, col: 0 },
+      { row: 0, col: 1 },
+      { row: 0, col: 2 },
+    ];
+    const movedSnake = move(snake, "RIGHT");
+    const newBoard = insertSnake(cells, movedSnake);
+    expect(newBoard).toEqual([
+      { row: 0, col: 0, isTail: false, isHead: false, isFood: false },
+      { row: 0, col: 1, isTail: true, isHead: false, isFood: false },
+      { row: 0, col: 2, isTail: true, isHead: false, isFood: false },
+      { row: 0, col: 3, isTail: false, isHead: true, isFood: false },
+      { row: 1, col: 0, isTail: false, isHead: false, isFood: false },
+      { row: 1, col: 1, isTail: false, isHead: false, isFood: false },
+      { row: 1, col: 2, isTail: false, isHead: false, isFood: false },
+      { row: 1, col: 3, isTail: false, isHead: false, isFood: false },
+      { row: 2, col: 0, isTail: false, isHead: false, isFood: false },
+      { row: 2, col: 1, isTail: false, isHead: false, isFood: false },
+      { row: 2, col: 2, isTail: false, isHead: false, isFood: false },
+      { row: 2, col: 3, isTail: false, isHead: false, isFood: false },
+      { row: 3, col: 0, isTail: false, isHead: false, isFood: false },
+      { row: 3, col: 1, isTail: false, isHead: false, isFood: false },
+      { row: 3, col: 2, isTail: false, isHead: false, isFood: false },
+      { row: 3, col: 3, isTail: false, isHead: false, isFood: false },
     ]);
   });
 });
