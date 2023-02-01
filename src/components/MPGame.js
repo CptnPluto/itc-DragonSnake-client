e0e0de9a68966c75b194749ae18384a416eeed0e
CptnPluto <cptnpluto@gmail.com>
2023-02-01 22:00:30 +0200

    Added MPGame sizing and whatnot.

--- src/components/MPGame.js
+++ src/components/MPGame.js
@@ -3,45 +3,86 @@ import "../components/Game.css";
 import { getDirectionFromKeyboard } from "../game_logic/snake";
 
 export default function MPGame({ initialCells, socket, roomId, playerNum }) {
-  const [cells, setCells] = useState(initialCells);
+    const [cells, setCells] = useState(initialCells);
 
-  socket.on("cells", (cells) => {
-    setCells(cells);
-  });
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      const arrowKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-      if (!arrowKeys.includes(e.key)) return;
-      const directionEntered = getDirectionFromKeyboard(e.key);
-      socket.emit("direction", {
-        directionEntered,
-        playerRoomId: roomId,
-        playerNum,
-      });
-    };
-    document.addEventListener("keydown", handleKeyDown);
-    return () => document.removeEventListener("keydown", handleKeyDown);
-  }, []);
+    socket.on("cells", (cells) => {
+        setCells(cells);
+    });
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            const arrowKeys = [
+                "ArrowUp",
+                "ArrowDown",
+                "ArrowLeft",
+                "ArrowRight",
+            ];
+            if (!arrowKeys.includes(e.key)) return;
+            const directionEntered = getDirectionFromKeyboard(e.key);
+            socket.emit("direction", {
+                directionEntered,
+                playerRoomId: roomId,
+                playerNum,
+            });
+        };
+        document.addEventListener("keydown", handleKeyDown);
+        return () => document.removeEventListener("keydown", handleKeyDown);
+    }, []);
 
-  return (
-    <div className="grid">
-      {cells &&
-        cells.map((cell) => {
-          return (
-            <div
-              key={cell.row.toString() + "-" + cell.col.toString()}
-              className={
-                cell.isHead
-                  ? "gridItem is-head"
-                  : cell.isTail
-                  ? "gridItem is-tail"
-                  : cell.isFood
-                  ? "gridItem is-food"
-                  : "gridItem"
-              }
-            ></div>
-          );
-        })}
-    </div>
-  );
+    return (
+        //     <div className="grid">
+        //       {cells &&
+        //         cells.map((cell) => {
+        //           return (
+        //             <div
+        //               key={cell.row.toString() + "-" + cell.col.toString()}
+        //               className={
+        //                 cell.isHead
+        //                   ? "gridItem is-head"
+        //                   : cell.isTail
+        //                   ? "gridItem is-tail"
+        //                   : cell.isFood
+        //                   ? "gridItem is-food"
+        //                   : "gridItem"
+        //               }
+        //             ></div>
+        //           );
+        //         })}
+        //     </div>
+        //   );
+        <div className="grid">
+            {cells.map((cell) => {
+                return (
+                    <div
+                        key={cell.row.toString() + "-" + cell.col.toString()}
+                        className={
+                            cell.isHead
+                                ? `gridItem is-head  ${direction}`
+                                : cell.isTail
+                                ? "gridItem is-tail"
+                                : cell.isFood
+                                ? "gridItem is-food"
+                                : "gridItem"
+                        }
+                    >
+                        <>
+                            {" "}
+                            {cell.isHead ? (
+                                <div className={`eyes`}>
+                                    <div className={`center`}>
+                                        <img src={eyes} className={`center`} />
+                                        <img
+                                            src={wings}
+                                            className={`center ${wingsSize}`}
+                                        />
+                                    </div>
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                        </>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
