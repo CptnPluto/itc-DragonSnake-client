787858a587bbd800528e16327262836f9e87bfec
CptnPluto <cptnpluto@gmail.com>
2023-02-01 20:29:16 +0200

    Fixed sound volume.

--- src/pages/SPGamepage.js
+++ src/pages/SPGamepage.js
@@ -23,7 +23,7 @@ const Gamepage = () => {
     const [count, setCount] = useState(0);
     const { user, render, setRender } = useAuthContext();
     const [playLose] = useSound(loseSound, { volume: 0.4 });
-    const [play, { stop }] = useSound(music);
+    const [play, { stop }] = useSound(music, { volume: 0.5 });
 
     const startMusic = () => {
         play();
