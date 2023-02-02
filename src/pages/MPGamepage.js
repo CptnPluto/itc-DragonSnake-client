b551965c3c8aded0f01b54a51d10cdbdc6038508
CptnPluto <cptnpluto@gmail.com>
2023-02-02 09:28:32 +0200

    Fixed MP modal

--- src/pages/MPGamepage.js
+++ src/pages/MPGamepage.js
@@ -16,119 +16,119 @@ import MPGame from "../components/MPGame";
 import { useSocket } from "../contexts/SocketContext";
 import { usePlayer } from "../contexts/PlayerContext";
 
-const MPGamepage = ({handleStartGame}) => {
-// const MPGamepage = ({ socket, initialCells, playerNum, roomId }) => {
-  const {playerNum, roomId} = usePlayer();
-    const [active, setActive] = useState(false);
-  const [message, setMessage] = useState("");
-  const socket = useSocket();
-  const [initialCells, setInitialCells] = useState([]);
-  // useEffect(() => {
+const MPGamepage = ({ handleStartGame }) => {
+    // const MPGamepage = ({ socket, initialCells, playerNum, roomId }) => {
+    const { playerNum, roomId } = usePlayer();
+    const [active, setActive] = useState(true);
+    const [message, setMessage] = useState("");
+    const socket = useSocket();
+    const [initialCells, setInitialCells] = useState([]);
+    // useEffect(() => {
     socket.on("start game", (cells) => {
-      initialCells.length && setActive(true);
-      setInitialCells(cells);
+        initialCells.length && setActive(true);
+        setInitialCells(cells);
     });
-  
-  socket.on("win", (winner) => {
-    setActive(false);
-    setMessage(`Player ${winner} wins!`);
-  });
-  // }, [socket]);
-  //   const [scoreMessage, setScoreMessage] = useState("");
-  //   const [score, setScore] = useState(0);
-  //   const [allScores, setAllScores] = useState([]);
-  //   const [count, setCount] = useState(0);
-  //   const { user, render, setRender } = useAuthContext();
-  //   const [play, { stop }] = useSound(music);
 
-  //   const startMusic = () => {
-  //     play();
-  //     setTimeout(() => {
-  //       if (active) play();
-  //     }, 147000);
-  //   };
+    socket.on("win", (winner) => {
+        setActive(false);
+        setMessage(`Player ${winner} wins!`);
+    });
+    // }, [socket]);
+    //   const [scoreMessage, setScoreMessage] = useState("");
+    //   const [score, setScore] = useState(0);
+    //   const [allScores, setAllScores] = useState([]);
+    //   const [count, setCount] = useState(0);
+    //   const { user, render, setRender } = useAuthContext();
+    //   const [play, { stop }] = useSound(music);
 
-  //   const increaseScore = () => {
-  //     switch (true) {
-  //       case score < 30:
-  //         setScore((prevScore) => prevScore + 10);
-  //         break;
-  //       case score > 100:
-  //         setScore((prevScore) => prevScore + 20);
-  //         break;
-  //       case score > 500:
-  //         setScore((prevScore) => prevScore + 50);
-  //         break;
-  //       case score > 3000:
-  //         setScore((prevScore) => prevScore + 100);
-  //         break;
-  //       default:
-  //         setScore((prevScore) => prevScore + 10);
-  //     }
+    //   const startMusic = () => {
+    //     play();
+    //     setTimeout(() => {
+    //       if (active) play();
+    //     }, 147000);
+    //   };
 
-  //     setCount((prevCount) => prevCount + 1);
-  //   };
-  //   const resetScore = () => {
-  //     setScore(0);
-  //     setCount(0);
-  //   };
+    //   const increaseScore = () => {
+    //     switch (true) {
+    //       case score < 30:
+    //         setScore((prevScore) => prevScore + 10);
+    //         break;
+    //       case score > 100:
+    //         setScore((prevScore) => prevScore + 20);
+    //         break;
+    //       case score > 500:
+    //         setScore((prevScore) => prevScore + 50);
+    //         break;
+    //       case score > 3000:
+    //         setScore((prevScore) => prevScore + 100);
+    //         break;
+    //       default:
+    //         setScore((prevScore) => prevScore + 10);
+    //     }
 
-  //   const postScore = async () => {
-  //     const newScore = {
-  //       date: new Date(),
-  //       score: score,
-  //       username: user.username,
-  //     };
-  //     try {
-  //       const res = await axios.post(
-  //         `${process.env.REACT_APP_SERVER_URL}/scores`,
-  //         newScore,
-  //         { withCredentials: true }
-  //       );
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //     setRender(!render);
-  //   };
+    //     setCount((prevCount) => prevCount + 1);
+    //   };
+    //   const resetScore = () => {
+    //     setScore(0);
+    //     setCount(0);
+    //   };
+
+    //   const postScore = async () => {
+    //     const newScore = {
+    //       date: new Date(),
+    //       score: score,
+    //       username: user.username,
+    //     };
+    //     try {
+    //       const res = await axios.post(
+    //         `${process.env.REACT_APP_SERVER_URL}/scores`,
+    //         newScore,
+    //         { withCredentials: true }
+    //       );
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //     setRender(!render);
+    //   };
 
     const handleLoss = () => {
-      setMessage("You lost! Try again?");
-      // setScoreMessage("Your score: " + score);
-      // postScore();
-      setActive(false);
-      // stop();
+        setMessage("You lost! Try again?");
+        // setScoreMessage("Your score: " + score);
+        // postScore();
+        setActive(false);
+        // stop();
     };
 
-  //   const getScores = async () => {
-  //     try {
-  //       const scores = await axios.get(
-  //         `${process.env.REACT_APP_SERVER_URL}/scores`,
-  //         { withCredentials: true }
-  //       );
-  //       setAllScores(scores.data);
-  //       console.log("Scores: ", scores.data);
-  //       function compareScores(a, b) {
-  //         if (a.score > b.score) {
-  //           return -1;
-  //         }
-  //         if (a.score < b.score) {
-  //           return 1;
-  //         }
-  //         return 0;
-  //       }
-  //       return scores.data.sort(compareScores);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+    //   const getScores = async () => {
+    //     try {
+    //       const scores = await axios.get(
+    //         `${process.env.REACT_APP_SERVER_URL}/scores`,
+    //         { withCredentials: true }
+    //       );
+    //       setAllScores(scores.data);
+    //       console.log("Scores: ", scores.data);
+    //       function compareScores(a, b) {
+    //         if (a.score > b.score) {
+    //           return -1;
+    //         }
+    //         if (a.score < b.score) {
+    //           return 1;
+    //         }
+    //         return 0;
+    //       }
+    //       return scores.data.sort(compareScores);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
-  //   useEffect(() => {
-  //     const res = getScores();
-  //   }, [render]);
+    //   useEffect(() => {
+    //     const res = getScores();
+    //   }, [render]);
 
-  return (
-    <div className="gamepage-container">
-      {/* <div
+    return (
+        <div className="gamepage-container">
+            {/* <div
         className="sideList"
         style={{ border: "2px    solid #FFFFFF1C", borderRight: 0 }}
       >
@@ -147,51 +147,51 @@ const MPGamepage = ({handleStartGame}) => {
         </ul>
       </div> */}
 
-      <div
-        className="mainRight"
-        style={{ border: "2px solid #FFFFFF1C", borderBottom: 0 }}
-      >
-        {!active && (
-          <GameModal
-            close={() => {
-              setActive(true);
-            }}
-          >
-            <div className="message">{message}</div>
-            {/* <div className="message score">{scoreMessage}</div> */}
-            <button
-              onClick={() => {
-                // resetScore();
-                handleStartGame()
-                setActive(true);
-                // startMusic();
-              }}
+            <div
+                className="mainRight"
+                style={{ border: "2px solid #FFFFFF1C", borderBottom: 0 }}
             >
-              Play again?
-            </button>
-          </GameModal>
-        )}
-        <div className="topBar">
-          {/* <h2>Score:</h2> <h2 className="bar-score">{score}</h2>
+                {!active && (
+                    <GameModal
+                        close={() => {
+                            setActive(true);
+                        }}
+                    >
+                        <div className="message">{message}</div>
+                        {/* <div className="message score">{scoreMessage}</div> */}
+                        <button
+                            onClick={() => {
+                                // resetScore();
+                                handleStartGame();
+                                setActive(true);
+                                // startMusic();
+                            }}
+                        >
+                            Play again?
+                        </button>
+                    </GameModal>
+                )}
+                <div className="topBar">
+                    {/* <h2>Score:</h2> <h2 className="bar-score">{score}</h2>
           <img src={trophy} className="bar-trophy" /> <h2>01</h2>
           <img src={coin} className="bar-coin" /> <h2>{count}</h2>
           <p className="userName">{user ? user.username : "Guest"}</p> */}
-          {/* <div className="userPic"> */}
-          {/* // </div> */}
-          <img src={userImg} className="userPic" />
-        </div>
+                    {/* <div className="userPic"> */}
+                    {/* // </div> */}
+                    <img src={userImg} className="userPic" />
+                </div>
 
-        <div className="gameField">
-          <MPGame
-            initialCells={initialCells}
-            // socket={socket}
-            roomId={roomId}
-            playerNum={playerNum}
-          />
+                <div className="gameField">
+                    <MPGame
+                        initialCells={initialCells}
+                        // socket={socket}
+                        roomId={roomId}
+                        playerNum={playerNum}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MPGamepage;
